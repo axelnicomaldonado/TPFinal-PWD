9258f2211e65a435fe0a25b09c1c7a52f124cca2
Julian Blanco <116972020+juliaanbl@users.noreply.github.com>
2023-11-14 02:50:19 -0300

    añadido estructura de home index.php

--- Vista/home/index.php
+++ Vista/home/index.php
@@ -1,1 +1,14 @@
-
+<?php
+include_once("../estructura/headerSeguro.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    //codigo para pagina principal
+</body>
+</html>
