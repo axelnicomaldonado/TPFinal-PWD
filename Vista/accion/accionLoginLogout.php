dabc3fb04989fc072f6277c646a76b3fb0164cab
Julian Blanco <116972020+juliaanbl@users.noreply.github.com>
2023-11-15 18:19:32 -0300

    actualizado accionLoginLogout.php

--- Vista/accion/accionLoginLogout.php
+++ Vista/accion/accionLoginLogout.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../estructura/cabeceraNoSegura.php");
+include_once("../../configuracion.php");
 $datos = data_submitted();
 $resp = false;
 $obj = new Session();
@@ -8,10 +8,20 @@ if (isset($datos['accion'])){
     if ($datos['accion']=="login"){
         $resp = $obj->iniciar($datos['usnombre'],$datos['uspass']);
         if($resp) {
-            echo("<script>location.href = '../home/index.php';</script>");
+            $listRol = $obj->getRol();
+            $i = 0;
+            while ($listRol[$i]->getobjusuario()->getidusuario() != $_SESSION["idusuario"]) {
+            $i++;
+            }
+            if($listRol[$i]->getobjusuario()->getidusuario() == 1 || $listRol[$i]->getobjusuario()->getidusuario() == 4){
+                echo("<script>location.href = '../home/index.php';</script>");
+            }
+            else{
+                echo("<script>location.href = '../home/panel.php';</script>");
+            }
         } else {
             $mensaje ="Error, vuelva a intentarlo";
-            echo("<script>location.href = '../login/index.php?msg=".$mensaje."';</script>");
+            echo("<script>location.href = '../login/login.php?msg=".$mensaje."';</script>");
         }
 
     }
@@ -20,7 +30,7 @@ if (isset($datos['accion'])){
     if ($datos['accion']=="cerrar"){
         $resp = $obj->cerrar();
         if($resp) {
-            echo("<script>location.href = '../login/index.php';</script>");
+            echo("<script>location.href = '../home/index.php';</script>");
         }
     }
 }
