38695eb1aeef3c1b35e6b8f18d4d63d1cf98da73
Nicolas <axel.maldonado@est.fi.uncoma.edu.ar>
2023-11-17 15:18:15 -0300

    vista de Mi cuenta

---
+++ Vista/cliente/cuentaCliente.php
@@ -0,0 +1,21 @@
+<?php
+$titulo = "Mi Cuenta";
+include_once("../../configuracion.php");
+include_once("../estructura/estCliente/headerCliente.php");
+$objUsuario = new AbmUsuario();
+$session = new Session();
+
+$data["idusuario"] = $session->getUsuario()->getidusuario();
+$arregloPerfil = $objUsuario->buscar($data);
+?>
+
+<div class="container d-flex justify-content-between flex-row " style="background-color: #fff; width: 90%; margin-top: 3em; border: 2px solid grey; height: 80%;">
+<div style="margin-top: 5em; margin-left: 1em" class="info-cuenta">
+<h2 style="position:relative; text-align: center; margin-top: 1.5em">Información de la cuenta</h2>
+    <p style="margin-top: 1em;"><b>Nombre de usuario: </b><?php echo $arregloPerfil[0]->getusnombre() ?>.</p>
+    <p style="margin-top: 1em;"><b>Correo Electrónico: </b><?php echo $arregloPerfil[0]->getusmail() ?></p>
+    <form action="editarCliente.php" method="post">
+      <button type="submit" class="btn btn-link">Cambiar datos</button>
+    </form>
+  </div>
+</div>
---
+++ Vista/cliente/editarCliente.php
@@ -0,0 +1,37 @@
+<?php
+$titulo = "Editar Cliente";
+include_once("../../configuracion.php");
+include_once("../estructura/estCliente/headerCliente.php");
+$session = new Session();
+
+$id = $session->getUsuario()->getidusuario();
+?>
+    <h3 class="mb-2" style="text-align: center; margin-bottom: 1em; margin-top: 1em;">Cambiar datos de la cuenta</h3>
+    <div style="margin: auto; border: 1px solid grey; padding: 1em; width: 40%;">
+        <form class="row g-3 needs-validation" method="get" action="accion\accionEditarCliente.php">
+            <div class="col-md-12">
+            <label for="usuario" class="form-label">Nuevo nombre de usuario</label>
+            <input type="text" class="form-control" id="usuario" required name="usuario">
+            <div id="usuarioInvalid" class="invalid-feedback">
+                Ingrese un usuario válido
+            </div>
+        </div>
+        <div class="col-md-12">
+            <label for="contrasenia" class="form-label">Nueva contraseña</label>
+            <input type="password" class="form-control" id="contrasenia" required name="contrasenia">
+            <div id="contraseniaInvalid" class="invalid-feedback">
+                La contraseña no es válida
+            </div>
+        </div>  
+        <div class="col-md-12">
+            <label for="correo" class="form-label">Nuevo correo</label>
+            <input type="text" class="form-control" id="correo" required name="correo">
+        </div>
+        <div id="correoInvalid" class="invalid-feedback">
+            Ingrese un correo válido
+        </div>
+            <div class="col-md-12 d-md-flex justify-content-md-center" style="margin-top:1em">
+            <button class=" btn btn-primary" type="submit">Editar datos</button>
+        </div>
+        <input type="text" name="id" id="id" hidden value="<?php echo $id; ?>">
+    </form>
