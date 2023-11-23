1de1f79c5d139a67f5f9df1f59063b8f3d71a9af
Nicolas <axel.maldonado@est.fi.uncoma.edu.ar>
2023-11-23 02:06:33 -0300

    navbar modificado

--- Vista/estructura/navbar.php
+++ Vista/estructura/navbar.php
@@ -37,16 +37,16 @@ foreach ($arregloRoles as $rol) {
 
     <nav class="navbar navbar-light bg-light sticky-top navbar-expand-lg">
     <div class="container-fluid max">
-        <a class="navbar-brand fw-bold" href="../Home/index.php"><img src="../img/logo_pizza.png" alt="" class="img-fluid mb-1" style="max-width:200px;"></a>
+        <a class="navbar-brand fw-bold" href="../Home/index.php">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse d-flex align-items-end justify-content-end" id="navbarNavDropdown">
+        <div class="collapse navbar-collapse d-flex align-items-end justify-content-start" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="btn text-dark" data-bs-toggle="offcanvas" href="#menu-dinamico" role="button" aria-controls="offcanvas"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" fill="currentColor" class="bi bi-list mb-1" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-                        </svg>&nbsp;Menu</a>
+                    <a class="btn text-dark" data-bs-toggle="offcanvas" href="#menu-dinamico" role="button" aria-controls="offcanvas">
+                    <i class="fa-solid fa-bars"></i> Menu
+                    </a>
                 </li>
             </ul>
         </div>
