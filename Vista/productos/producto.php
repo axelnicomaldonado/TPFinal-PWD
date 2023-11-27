bb6e69a0e03800a3e6c74a05defa7437b23e3058
Nicolas <axel.maldonado@est.fi.uncoma.edu.ar>
2023-11-26 21:46:09 -0300

    Header incluido en producto

--- Vista/productos/producto.php
+++ Vista/productos/producto.php
@@ -3,22 +3,10 @@
 $titulo = 'Detalle del producto';
 include_once("../../configuracion.php");
 
-
 $obj = new Session();
 $objUserRol = new AbmUsuarioRol();
 $resp = $obj->validar();
-$listRol = $obj->getRol();
-if($resp) {
-    if($listRol[0]->getobjrol()->getIdRol() == 1){
-        include_once("../estructura/estCliente/headerCliente.php");
-    }
-    else{
-        $mensaje ="Error, acceso solo Admin. Inicie sesion como admin para continuar";
-        echo("<script>location.href = '../login/login.php?msg=".$mensaje."';</script>");
-    }
-} else {
-    include_once("../estructura/estPublico/headerPublico.php");
-}
+include_once("../estructura/headerInseguro.php");
 
 
 
@@ -84,10 +72,5 @@ if ($id == '' || $token == '') {
 
 
         </div>
-            <?php
-            $id = session_id();
-            echo $id;
-
-            ?>
     </div>
 </main>
